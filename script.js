3f9e81395b25fc6ca7a44325c81fe33ddb12d877
Kiyotaka <2804629450@qq.com>
2025-02-03 00:34:10 +0800

    Updata commit

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ toGithubBtn.addEventListener(
         window.location.href="https://github.com/K1y0t4k4";
     }
 );
+
+const hrefs = ["https://www.facebook.com", "https://x.com", "https://www.instagram.com", "https://www.youtube.com"]
+const icons = document.querySelectorAll("[icon]");
+for (let i=0; i < icons.length; i++) {
+    icons[i].addEventListener(
+        "click",
+        function () {
+            window.location.href=hrefs[i]
+        }
+    );
+}
