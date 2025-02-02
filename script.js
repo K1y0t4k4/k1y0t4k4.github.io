237cb25f8a9fb3ada028f0e593f0240185efcb75
Kiyotaka <2804629450@qq.com>
2025-02-03 00:20:51 +0800

    Updata commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const toGithubBtn = document.querySelector("[to-github-btn]");
+toGithubBtn.addEventListener(
+    "click",
+    function () {
+        window.location.href="https://github.com/K1y0t4k4";
+    }
+);
